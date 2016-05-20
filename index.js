6a4dc0ee727e100edee0c19cc57899230f0c50c8
Robert Helmer <rhelmer@rhelmer.org>
2016-05-20 14:53:02 -0700

    Rename panel.{html,css,js} to report, remove unused deps, declare multiprocess compat

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const { ToggleButton } = require("sdk/ui/button/toggle");
 const tabs = require("sdk/tabs");
-const self = require("sdk/self");
-const data = require("sdk/self").data;
 const { window: { document } } = require('sdk/addon/window');
-const { getTabContentWindow, getActiveTab } = require('sdk/tabs/utils');
-const { getMostRecentBrowserWindow } = require('sdk/window/utils');
+const { viewFor } = require("sdk/view/core");
 
 // Canvas is used for taking screen shots.
 const canvas = document.createElementNS('http://www.w3.org/1999/xhtml', 'canvas');
@@ -12,10 +8,8 @@ document.documentElement.appendChild(canvas);
 
 // This add-on uses built-in tracking protection, which doesn't have
 // hooks for add-ons to use yet.
-const {Cc, Ci, Cu} = require("chrome");
+const {Cu} = require("chrome");
 Cu.import("resource://gre/modules/Services.jsm");
-const wm = Cc["@mozilla.org/appshell/window-mediator;1"]
-             .getService(Ci.nsIWindowMediator);
 
 // Enable tracking protection globally when add-on is enabled.
 Services.prefs.setBoolPref("privacy.trackingprotection.enabled", true);
@@ -54,16 +48,23 @@ function attachScreenshot(report) {
   browserMM.sendAsyncMessage('fs/screenshot', report);
 }
 
-function infobar(message) {
-  let mainWindow = wm.getMostRecentWindow("navigator:browser");
+let reportPanel = require("sdk/panel").Panel({
+  contentURL: require("sdk/self").data.url("report.html")
+});
 
-  // remove any active infobars for this tab
+function clearInfobar(win) {
   try {
-    mainWindow.gBrowser.getNotificationBox().removeCurrentNotification();
+    viewFor(win).gBrowser.getNotificationBox().removeCurrentNotification();
   } catch(e) {
     // ok if there are is currently no infobar
   }
-  let notificationBox = mainWindow.gBrowser.getNotificationBox();
+}
+
+function infobar(win, message) {
+  // remove any active infobars for this tab
+  clearInfobar(win);
+
+  let notificationBox = viewFor(win).gBrowser.getNotificationBox();
   let buttons = [{
     label: "Disable",
     callback: function () {
@@ -76,7 +77,7 @@ function infobar(message) {
   }, {
     label: "Report",
     callback: function () {
-      console.log("report a problem");
+      reportPanel.show();
     }
   }];
   let notificationBar = notificationBox.appendNotification(
@@ -86,25 +87,32 @@ function infobar(message) {
   notificationBar.persistence = 1;
 }
 
-function activateTrackingProtection() {
+function activateTrackingProtection(win) {
   let normalizedUrl = normalizeUrl(tabs.activeTab.url);
-  let mainWindow = wm.getMostRecentWindow("navigator:browser");
 
   // TODO is there a more direct way to tell if there are tracking elements
   //      on the current page?
-  let elem = mainWindow.document.getElementById("tracking-blocked");
+  let elem = viewFor(win).document.getElementById("tracking-blocked");
 
   if (normalizedUrl.scheme != "https") {
     console.log("tracking protection only works for web URLs");
   } else if (Services.perms.testPermission(normalizedUrl, "trackingprotection")) {
     console.log("tracking protection already disabled for this URL");
-  } else if (mainWindow.getComputedStyle(elem,null).getPropertyValue("display") == "none") {
+  } else if (viewFor(win).getComputedStyle(elem,null).getPropertyValue("display") == "none") {
     console.log("no tracking elements found on this page");
   } else {
-    infobar(`Tracking Protection active for ${normalizedUrl.host}, how does this page look?`);
+    infobar(win, `Tracking Protection active for ${normalizedUrl.host}, how does this page look?`);
   }
 }
 
-tabs.on("ready", () => {
-  activateTrackingProtection();
+tabs.on("create", (tab) => {
+  clearInfobar(tab.window);
+});
+
+tabs.on("ready", (tab) => {
+  activateTrackingProtection(tab.window);
+});
+
+tabs.on("*", (evt) => {
+  console.log("rhelmer debug", evt);
 });
