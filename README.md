b83a14f0337473877bf707631a46eb6f188562f1
Robert Helmer <rhelmer@rhelmer.org>
2016-05-22 14:40:12 -0700

    convert from jetpack to plain bootstrap extension

--- README.md
+++ README.md
@@ -5,22 +5,26 @@ Enables Firefox's built-in Tracking Protection features, and provides
 the ability to quickly and easily disable/enable and provide comments
 and screenshots on sites that don't work correctly.
 
+Installing this add-on will:
+
+ 1) disable any built-in adblockers
+ 2) automatically enable tracking protection (via the pref)
+ 3) show a low-priority infobar when tracking elements are detected.
+
+The infobar prompts users to report a problem (with optional screenshot).
+
+ TODO implement report page (overlay content)
+ TODO remember sites which have already been reported or infobar dismissed
+ TODO add report invocation button to control center location
+ TODO either prompt for permission or notify about disabling existing adblockers
+
 # Requirements
 
   Node version 5+
   Firefox 45+
-
-## Install Firefox add-on tool jpm
-  npm install jpm -g
 
 ## Install test collection server
   npm install
 
 ## Run test collection server (in background)
   npm start &
-
-## Run Firefox with tracking add-on
-  jpm run
-
-## Generate XPI file for installing, uploading to addons.mozilla.org etc.
-  jpm xpi
---
+++ bootstrap.js
@@ -0,0 +1,100 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { utils: Cu, interfaces: Ci, classes: Cc } = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/AddonManager.jsm");
+
+const ADBLOCKERS = ["uBlock0@raymondhill.net"];
+
+function showInfobar(gBrowser) {
+  let notificationBox = gBrowser.getNotificationBox();
+  let notification = notificationBox.getNotificationWithValue("stop-tracking-request");
+  // TODO clear any infoboxes for previous sites loaded in this tab
+  if (!notification) {
+    let message = `Tracking Protection active for ${gBrowser.currentURI.host}, how does this page look?`;
+    let buttons = [{
+      label: "Report a problem",
+      callback: function () {
+        console.log("report problem with tracking protection");
+        // TODO hook up report
+      }
+    }];
+    let notificationBar = notificationBox.appendNotification(
+                            message, "stop-tracking-request", "",
+                            notificationBox.PRIORITY_INFO_LOW,
+                            buttons);
+    notificationBar.persistence = 1;
+  }
+}
+
+function attachInfobar(aWindow) {
+  let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindow);
+  domWindow.addEventListener("load", function () {
+    domWindow.removeEventListener("load", arguments.callee, false);
+    if (domWindow.gBrowser && domWindow.gBrowser.tabContainer) {
+      let gBrowser = domWindow.gBrowser;
+      gBrowser.tabContainer.addEventListener("load", function (evt) {
+        let doc = evt.target.ownerDocument;
+        let win = evt.target.ownerGlobal;
+        let elem = doc.getElementById("tracking-blocked");
+        if (win.getComputedStyle(elem).getPropertyValue("display") != "none") {
+          showInfobar(gBrowser);
+        }
+      }, true);
+    }
+  }, false);
+};
+
+let windowListener = {
+  onOpenWindow: aWindow => {
+    attachInfobar(aWindow);
+  },
+};
+
+function startup() {
+  // disable existing ad blockers
+  AddonManager.getAddonsByTypes(["extension"], addons => {
+    for (addon of addons) {
+      if (addon && addon.id in ADBLOCKERS) {
+        // TODO should notify users that adblockers are disabled
+        console.log(`disabling ad blocker ${addon.id}`);
+        addon.userDisabled = true;
+      }
+    }
+  });
+
+  // enable built-in tracking protection
+  Services.prefs.setBoolPref("privacy.trackingprotection.enabled", true);
+
+  // attach listener to existing browser windows
+  let windows = wm.getEnumerator("navigator:browser");
+  while (windows.hasMoreElements()) {
+    // TODO also attach to existing tabs?
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    attachInfobar(domWindow);
+  }
+
+  // attach listener to any new windows
+  Services.wm.addListener(windowListener);
+}
+function shutdown() {
+  // disable built-in tracking protection
+  Services.prefs.setBoolPref("privacy.trackingprotection.enabled", false);
+
+  // remove listener from existing windows
+  while (windows.hasMoreElements()) {
+    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+    WindowListener.setupBrowserUI(domWindow);
+  }
+
+  // stop listening for new windows
+  Services.wm.removeListener(windowListener);
+
+  // TODO re-enable disabled ad blockers
+}
+function install() {}
+function uninstall() {}
