e1d0d6f5290d5f44310ac99480f3eaf99b211f09
Robert Helmer <rhelmer@rhelmer.org>
2016-05-23 19:42:38 -0700

    package up chrome.manifest too

--- firefox/package.sh
+++ firefox/package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-zip -r "stop-tracking@test.mozilla.org.xpi" install.rdf bootstrap.js content/
+zip -r "stop-tracking@test.mozilla.org.xpi" install.rdf bootstrap.js chrome.manifest content/
